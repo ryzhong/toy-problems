e669b079571410ef0b47b5259b73391b8b182087
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2019-12-10 21:46:16 -0800

    First Attempt 10%

---
+++ disappearedNums.js
@@ -0,0 +1,14 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+var findDisappearedNumbers = function(nums) {
+    let result = [];
+    for(let i = 1; i < nums.length + 1; i++) {
+        if(nums.indexOf(i) === -1 ) {
+            result.push(i);
+        }
+    }
+    // console.log(result);
+    return result;
+};
