b9d6e0b6efa94174388c7adaa5a9eb6b0727d0dd
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2019-12-11 21:06:57 -0800

    First Attempt 78%

---
+++ 4sumII.js
@@ -0,0 +1,32 @@
+// /**
+//  * @param {number[]} A
+//  * @param {number[]} B
+//  * @param {number[]} C
+//  * @param {number[]} D
+//  * @return {number}
+//  */
+var fourSumCount = function(A, B, C, D) {
+    let result = 0;
+    
+    const combineTwoSets = (set1, set2) => {
+        let curMap = new Map();
+        for(let i = 0; i < set1.length; i++) {
+            for(let j = 0; j < set2.length; j++) {
+                let sum = set1[i] + set2[j];
+                curMap.set(sum, curMap.get(sum) + 1 || 1);
+            }
+        }
+        return curMap;
+    }
+    
+    let set1 = combineTwoSets(A,B);
+    let set2 = combineTwoSets(C,D);
+    
+    set1.forEach((val, key) => {
+        let offset = key * -1;
+        if(set2.get(offset)) {
+            result += set2.get(offset) * set1.get(key);
+        }
+    })
+    return result;
+};
