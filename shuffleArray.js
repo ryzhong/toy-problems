fd371688746a33bd02ce1d34252076b0082e771a
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2019-12-18 20:40:26 -0800

    First Attempt 26%

---
+++ shuffleArray.js
@@ -0,0 +1,44 @@
+// /**
+//  * @param {number[]} nums
+//  */
+var Solution = function(nums) {
+    this.o = nums.slice();
+    this.arr = nums;
+    // console.log(this.arr)
+};
+
+/**
+ * Resets the array to its original configuration and return it.
+ * @return {number[]}
+ */
+Solution.prototype.reset = function() {
+    // console.log('reseting')
+    // console.log(this.o)
+    return this.o;
+};
+
+/**
+ * Returns a random shuffling of the array.
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function() {
+    // console.log('shuffling')
+    let shuffling = this.arr;
+    shuffling.forEach((val, index) => {
+        let randomIndex = Math.floor(Math.random() * shuffling.length);
+        let randomIndexVal = shuffling[randomIndex];
+        // console.log( randomIndex)
+        //switch places
+        shuffling[index] = randomIndexVal;
+        shuffling[randomIndex] = val;
+    })
+    // console.log('shuffled', shuffling)
+    return shuffling;
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
