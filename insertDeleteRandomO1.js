aa22ea922d1edb8d5b935c310280ae4dc9d2c369
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2019-12-18 22:00:16 -0800

    Third Attempt 38%

--- insertDeleteRandomO1.js
+++ insertDeleteRandomO1.js
@@ -89,3 +89,63 @@ RandomizedSet.prototype.getRandom = function() {
     return keys[randomIndex];
 
 };
+
+//38% using both array and hashmap
+/**
+ * Initialize your data structure here.
+ */
+var RandomizedSet = function() {
+    this.arr = [];
+    this.setMap = new Map();
+};
+
+/**
+ * Inserts a value to the set. Returns true if the set did not already contain the specified element. 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    let contains = this.arr.includes(val);
+    if(contains) return false;
+    this.arr.push(val);
+    this.setMap.set(val, this.arr.length - 1);
+    // console.log('insert', this.setMap)
+    return true;
+};
+
+/**
+ * Removes a value from the set. Returns true if the set contained the specified element. 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    let contains = this.arr.includes(val);
+    if(!contains) return false;
+    if(this.setMap.get(val) < this.arr.length - 1){
+        let last = this.arr[this.arr.length - 1]
+        this.setMap.set(last, this.setMap.get(val));
+        this.arr[this.setMap.get(val)] = last;
+    }
+    this.setMap.delete(val);
+    this.arr.pop();
+    // console.log('pop', this.setMap)
+    return true;
+};
+
+/**
+ * Get a random element from the set.
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    let randomNum = Math.floor(Math.random() * this.arr.length);
+    // console.log('random', this.setMap)
+    return this.arr[randomNum]
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
