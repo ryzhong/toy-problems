b379ad80d9554ff8aeaaf2742b853d7f7266da86
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2019-12-18 20:06:01 -0800

    Better solution 97%

--- firstUniqueChar.js
+++ firstUniqueChar.js
@@ -26,15 +26,26 @@
 //  */
 
 //a bit cleaner 53%
+// var firstUniqChar = function(s) {
+//     let counter = new Map();
+//     for(let i = 0; i < s.length; i++) {
+//         counter.set(s[i], counter.get(s[i]) > 0 ? counter.get(s[i]) + 1 : 1 );
+//     }
+//     for(const [key, val] of counter.entries()) {
+//         if(val === 1) {
+//             return s.indexOf(key)
+//         }
+//     }
+//     return -1;
+// };
+
+//97%
+
 var firstUniqChar = function(s) {
-    let counter = new Map();
-    for(let i = 0; i < s.length; i++) {
-        counter.set(s[i], counter.get(s[i]) > 0 ? counter.get(s[i]) + 1 : 1 );
-    }
-    for(const [key, val] of counter.entries()) {
-        if(val === 1) {
-            return s.indexOf(key)
-        }
+    for(i=0;i<s.length;i++){
+        if (s.indexOf(s[i])===s.lastIndexOf(s[i])){
+           return i;
+       } 
     }
     return -1;
-};
+ };
