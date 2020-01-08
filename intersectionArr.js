500f6867d192a45fdf79874ac185aeb31966924e
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2020-01-07 22:38:47 -0800

    Problem

---
+++ intersectionArr.js
@@ -0,0 +1,12 @@
+// /**
+//  * @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @return {number[]}
+//  */
+
+
+var intersect = function(nums1, nums2) {
+    nums1.forEach( (ele) => {
+        
+    })
+};
