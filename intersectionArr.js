63be6a61da52dc13b336496218f56ba1b6597daf
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2020-01-08 23:45:36 -0800

    First Attempt

--- intersectionArr.js
+++ intersectionArr.js
@@ -6,7 +6,13 @@
 
 //test
 var intersect = function(nums1, nums2) {
+    let result = [];
     nums1.forEach( (ele) => {
-        
+        for(let i = 0; i < nums2.length; i++) {
+            if(ele === nums2[i]) {
+                result.push(ele);
+            }
+        }
     })
+    return result;
 };
