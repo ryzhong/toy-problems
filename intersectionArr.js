8a46e1c8c8d0e3ed6f2249e0271840075013abf3
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2020-01-14 19:03:12 -0800

    Better solution 86%

--- intersectionArr.js
+++ intersectionArr.js
@@ -19,3 +19,20 @@ var intersect = function(nums1, nums2) {
     })
     return result;
 };
+
+// 21%
+
+var intersect = function(nums1, nums2) {
+    let result = [];
+    let shortest = nums1.length <= nums2.length ? nums1 : nums2;
+    let longest = nums1.length <= nums2.length ? nums2 : nums1;
+    console.log( shortest, longest)
+    shortest.forEach( (ele, index) => {
+        let contains = longest.indexOf(ele);
+        if( contains >= 0) {
+            result.push(ele);
+            longest[contains] = null;
+        }
+    })
+    return result;
+};
