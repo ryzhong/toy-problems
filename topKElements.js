ab7d78bd902180958cd65c2960c2c8d4f26d2e44
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2020-01-22 22:52:46 -0800

    Second Attempt 98% but uses more memory

--- topKElements.js
+++ topKElements.js
@@ -31,3 +31,51 @@ var topKFrequent = function(nums, k) {
     return result
     
 };
+
+// 98%
+
+var topKFrequent = function(nums, k) {
+    let map = new Map();
+    let result = [];
+    nums.forEach( e => {
+        if(map.get(e)) {
+            map.set(e, map.get(e) + 1);
+        } else {
+            map.set(e, 1);
+        }
+    })
+    // while(result.length < k) {
+    //     // console.log(map)
+    //     let max = 0;
+    //     let num = 0;
+    //     map.forEach( (counter, ele) => {
+    //         // console.log(counter, ele);
+    //         if(max < counter) {
+    //             max = counter;
+    //             num = ele;
+    //         }
+    //     })
+    //     // console.log('result', num)
+    //     result.push(num);
+    //     map.delete(num);
+    // }
+    let x = new Map([...map].sort((a,b) => {
+        return b[1] - a[1];
+    }));
+   // Some sort function comparing keys with a[0] b[0] or values with a[1] b[1]
+   // Be sure to return -1 if lower and, if comparing values, return 0 if equal
+        // (a[0] < b[0] && 1) || a[0] === b[0] ? 0 : -1)
+    
+    // console.log(result);
+    // return result;
+    // for(let i = 0; i < k; i++) {
+    //     result.push(map.get)
+    // }
+    x = [...x]
+    // console.log(x)
+    for(let i = 0; i < k; i++) {
+        result.push(x[i][0]);
+    }
+    return result;
+    
+};
